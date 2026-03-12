48516bda9110b92eca3b92afd5ec100ad8e57650
aasthaanandcognizant <Aastha.Anand@cognizant.com>
2026-03-12 20:57:24 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/cognizant/civicaid/CivicaidApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.civicaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/civicaid/CivicaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.civicaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
