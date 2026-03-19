f27a47f1aef173b9f5f3ed071e24c2945b7ef548
Kanishka-Priya-work <Kanishkapriya.Manikandan@cognizant.com>
2026-03-19 18:18:06 +0530

    Adding EligibilityCheck classes

---
+++ src/main/java/com/cognizant/civicaid/service/EligibilityCheckService.java
@@ -0,0 +1,12 @@
+package com.cognizant.civicaid.service;
+
+import com.cognizant.civicaid.dto.request.EligibilityCheckRequest;
+import com.cognizant.civicaid.dto.response.EligibilityCheckResponse;
+
+import java.util.List;
+
+public interface EligibilityCheckService {
+    EligibilityCheckResponse performCheck(Long officerId, EligibilityCheckRequest request);
+    List<EligibilityCheckResponse> getChecksByApplication(Long applicationId);
+    EligibilityCheckResponse getLatestCheck(Long applicationId);
+}
