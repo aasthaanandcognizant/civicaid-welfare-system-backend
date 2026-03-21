b9408dd0626b8a8236cc913d0f2cb73666d57882
Aman kumar <2478411@cognizant.com>
2026-03-22 01:19:25 +0530

    updating the roles data-types

--- src/main/java/com/cognizant/civicaid/dto/request/SignUpRequestDto.java
+++ src/main/java/com/cognizant/civicaid/dto/request/SignUpRequestDto.java
@@ -1,6 +1,5 @@
 package com.cognizant.civicaid.dto.request;
 
-import com.cognizant.civicaid.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +12,7 @@ public class SignUpRequestDto {
     private String name;
     private String password;
     private String email;
-    private Role role;
+    private String role;
     private String phone;
     private String status;
 
